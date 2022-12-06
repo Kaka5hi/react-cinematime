18df8db5c7a7f438db91b8da2d62cf8e699b2d47
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2022-12-06 11:21:10 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import ContentSection from '../components/Content-section/ContentSection'
 import HeroSection from '../components/Hero-section/HeroSection'
+import Footer from '../components/Footer/Footer'
 
 const Home = () => {
+
+    React.useEffect(() => {
+        window.scrollTo(0, 0)
+    },[])
+
     return (
-        <main>
-            <HeroSection />
-            <ContentSection type={'movie'} heading={'movies'} />
-            <ContentSection type={'tv'} heading={'tv shows'}/>
-        </main>
+        <>
+            <main>
+                <HeroSection />
+                <ContentSection type={'movie'} heading={'movies'} />
+                <ContentSection type={'tv'} heading={'tv shows'}/>
+            </main>
+            <Footer />
+        </>
     )
 }
 
